109308e7da3d22ae5234972b397778ecdaa6537b
Pavlo Kysliak <pavel.kis0505@gmail.com>
2022-11-09 11:59:20 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
 export const App = () => {
+  state = {
+    board: [
+      [0, 2, 3, 0],
+      [2, 2, 1, 0],
+      [0, 0, 0, 0],
+    ],
+  };
+  // return (
+  //   <div
+  //     style={{
+  //       height: '100vh',
+  //       display: 'flex',
+  //       justifyContent: 'center',
+  //       alignItems: 'center',
+  //       fontSize: 40,
+  //       color: '#010101'
+  //     }}
+  //   >
+
+  const { board } = this.state;
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      {board.map((row, i) => (
+        <div key={i}>
+          {row.map((col, j) => (
+            <span key={j}>{col}</span>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
